ae20a02fcc358a7dab834ce3fea6bb7fdcf3b180
Zetong Yang <tonythemayfly@gmail.com>
2021-04-24 19:53:07 +0800

    moves css to external script, changes flask code to cope

--- app.py
+++ app.py
@@ -14,13 +14,17 @@ def index_get():
 def vuejs_get():
     return send_from_directory(pwd, "vue_app/vue.js")
 
-@app.route('/main.js', methods=['GET'])
-def mainjs_get():
-    return send_from_directory(pwd,  "vue_app/main.js")
+@app.route('/script.js', methods=['GET'])
+def scriptjs_get():
+    return send_from_directory(pwd,  "vue_app/script.js")
 
 @app.route('/bootstrap.css', methods=['GET'])
 def bootstrapcss_get():
     return send_from_directory(pwd, "vue_app/bootstrap.css")
+
+@app.route('/style.css', methods=['GET'])
+def stylecss_get():
+    return send_from_directory(pwd, "vue_app/style.css")
 
 
 
