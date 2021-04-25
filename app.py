1fb5eaa0892480aa5aab204008665b98487d95fe
Zetong Yang <tonythemayfly@gmail.com>
2021-04-25 09:52:00 +0800

    implements login and logout

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask, send_from_directory
+from flask_restful import Resource, Api,reqparse
 from os import path
 
 app = Flask(__name__)
 
+api = Api(app)
+parser = reqparse.RequestParser()
+
 pwd = path.dirname(path.realpath(__file__))
+users = {"test": 1}
+rooms = {
+    "Public": [],
+    1: ["test"]
+}
 
 # return the full app
 @app.route('/', methods=['GET'])
@@ -26,7 +35,31 @@ def bootstrapcss_get():
 def stylecss_get():
     return send_from_directory(pwd, "vue_app/style.css")
 
+class Login(Resource):
+    def post(self):
+        parser.add_argument("username",type=str)
+        username = parser.parse_args()["username"]
+        if username in users.keys():
+            return {"status": 0}
+        else:
+            users[username] = "Public"
+            rooms["Public"].append(username)
+            print("new user " + username)
+            return {"status": 1}
 
+class Logout(Resource):
+    def post(self):
+        parser.add_argument("username",type=str)
+        username = parser.parse_args()["username"]
+        if username in users.keys():
+            room = users.pop(username)
+            rooms[room].remove(username)
+            if ((room != "Public") and (rooms[room] == [])):
+                del rooms[room]
+            return {"status": 1}
+
+api.add_resource(Login,"/login")
+api.add_resource(Logout,"/logout")
 
 if __name__ == "__main__":
     app.run("127.0.0.1", 5000, True)
--- vue_app/script.js
+++ vue_app/script.js
@@ -1,7 +1,44 @@
 var app = new Vue({
     el: '#app',
     data: {
-      loggedIn: true,
-      myId: "hey"
+      loggedIn: false,
+      loginAlert: "",
+      myId: "",
+      myRoom:"",
+      activeUsers: [],
+      loginInput: ""
+    },
+    methods: {
+      login: function() {
+        var self = this;
+        axios.post("/login", {
+          username: this.loginInput
+        })
+        .then(function(response) {
+          if (response.data.status == 1) {
+            console.log("post response returned");
+            self.myId = self.loginInput;
+            self.loginInput = "";
+            self.loggedIn = true;
+            self.myRoom = "Public";
+          } else {
+            self.loginAlert = "Duplicated username, please try again."
+          }
+        });
+      },
+      logout: function() {
+        var self = this;
+        axios.post("/logout", {
+          username: this.myId
+        })
+        .then(function(response) {
+          if (response.data.status == 1) {
+            console.log("user deleted");
+            self.myId = "";
+            self.myRoom = "";
+            self.loggedIn = false;
+          }
+        })
+      }
     }
   })
